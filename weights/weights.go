75ca1a48944d3403f19e8dbe9d659c5290c38929
Ayaz Ayupov <ayaz@releaseband.com>
2021-04-26 15:55:57 +0300

    GRP-633 #time 2m added new models

--- weights/weights.go
+++ weights/weights.go
@@ -9,3 +9,15 @@ type Weights interface {
 type WeightGetter interface {
 	Get(belongs, weightName string, groupKey uint16) Weights
 }
+
+
+// [min,max]
+type rng func(min uint32, max uint32) uint32
+
+type GroupedWeights interface {
+	Value(groupName string, rng rng) (uint16, error)
+}
+
+type GroupedWeightsGetter interface {
+	Get(belongs, weightName string, groupKey uint16) GroupedWeights
+}
