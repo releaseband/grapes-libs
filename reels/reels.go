983605fec9f885a615ba918459cf22c9de004b1b
Ayaz Ayupov <ayaz@releaseband.com>
2021-07-15 10:16:45 +0300

    GRP-732 added buy feature reels generator

--- reels/reels.go
+++ reels/reels.go
@@ -13,16 +13,24 @@ type ReelsGenerator interface {
 	Generate(rng RNG) ([][]uint32, error)
 }
 
+type BuyFeatureReelsGenerator interface {
+	Type
+	Generator(rng RNG, bfType uint16) ([][]uint32, error)
+}
+
+//deprecated
 type Generator interface {
 	Type
 	Generate(ids []int) [][]uint32
 }
 
+//deprecated
 type Group interface {
 	Exp() float64
 	Generators() []Generator
 }
 
+//deprecated
 type Getter interface {
 	Get(name string, key uint8) Group
 }
