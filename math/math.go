4ccac103e7f68b5ca802c6a7344818395f4ad56a
Ayaz Ayupov <ayaz@releaseband.com>
2021-07-16 08:43:51 +0300

    GRP-732 changed Math, added store

--- math/math.go
+++ math/math.go
@@ -10,5 +10,9 @@ type Math interface {
 	EXP() float64
 	BuyFeatureReelsGroup() reels.BuyFeatureReelsGroup
 	SimpleReelsGroup() reels.SimpleReelsGroup
-	Weights() weights.Weights
+	Weights() []weights.SimpleWeights
+}
+
+type Store interface {
+	Get(name string) ([]Math, error)
 }
