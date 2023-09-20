97c282b2e32b38302fce8bc2c48b8b22a32bc0ef
Ayaz Ayupov <ayaz@releaseband.com>
2023-09-20 13:07:58 +0700

    fix: add rng lib;

--- reels/group.go
+++ reels/group.go
@@ -1,16 +1,19 @@
 package reels
 
+// deprecated
 type BuyFeatureReelsGroup interface {
 	BuyFeatureReels() (BuyFeatureReelsGenerator, error)
 	BonusReels() (ReelsGenerator, error)
 	ExtendedBonusReels() (ExtendedReels, error)
 }
 
+// deprecated
 type SimpleReelsGroup interface {
 	BaseReels() (ReelsGenerator, error)
 	BonusReels() (ReelsGenerator, error)
 }
 
+// deprecated
 type ExtendedReelsGroup interface {
 	SimpleBaseReels() (ReelsGenerator, error)
 	BaseReels() (ExtendedReels, error)
